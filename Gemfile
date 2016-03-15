8bed735f6baccee07434221be5852811906e115b
Brian Hildebrandt <brian_hildebrandt@hotmail.com>
2016-03-15 16:52:05 -0400

    second commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+# If you do not have OpenSSL installed, update
+# the following line to use "http://" instead
+source 'https://rubygems.org'
+ruby '2.3.0'
+
+gem "middleman", "~>3.3.12"
+
+# Live-reloading plugin
+gem "middleman-livereload", "~> 3.1.0"
+
+# Deploy to GitHub Pages
+gem "middleman-deploy"
+
+# Assets
+gem "bootstrap-sass", require: false
+gem "jquery-middleman"
+
+gem 'middleman-search_engine_sitemap'
+
+gem 'font-awesome-sass', '~> 4.2.0'
+gem 'middleman-minify-html', '~>  3.4.1'
+gem 'middleman-imageoptim'
---
+++ source/javascript/_smooth-scroll.js
@@ -0,0 +1,40 @@
+(function($) {
+    "use strict";
+
+    // Smooth Scrolling: Smooth scrolls to an ID on the current page.
+
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Activates floating label headings for the contact form.
+    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+    }).on("focus", ".floating-label-form-group", function() {
+        $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function() {
+        $(this).removeClass("floating-label-form-group-with-focus");
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Scrollspy: Highlights the navigation menu items while scrolling.
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    })
+
+})(jQuery); // End of use strict
+
+
+    jQuery(document).ready(function() {
+        App.init();
+        FancyBox.initFancybox();
+    });
